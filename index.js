e16397095c895e2d69f8975f74ffea3c189f5bbb
Jeffrey Gensler <jgensler8@gmail.com>
2014-11-22 15:40:12 -0600

    Adding connection strings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kafka = require('kafka-node'),
     Producer = kafka.Producer,
-    client = new kafka.Client(),
+    client = new kafka.Client("zookeeper:2182/kafka0.8", "kafka-node-client", {});
     producer = new Producer(client),
     payloads = [
         { topic: 'fridge', messages: "{'temp':50, 'mopened': [1,3], 'tOpened': 12, 'tClosed': 14}" },
