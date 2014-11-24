ab07913976eb333031889e6ec5b3fc748fb35639
Jeffrey Gensler <jgensler8@gmail.com>
2014-11-23 19:12:25 -0600

    adding random mode

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ commander
   .option('-id, --id [value]', 'ID for this producer')
   .option('-kh, --kafka-domain [value]', 'iKafka idomain to connect to.')
   .option('-kp, --kafka-port [n]', 'Kafka port to cionnect to.', parseInt)
+  .option('-emu, --emulated', 'Run in emulated mode (no serial reading, random data)')
   .parse(process.argv);
 
 console.log(commander.kafkaDomain, commander.kafkaPort, commander.id);
@@ -19,10 +20,11 @@ var kafkaesque = require('kafkaesque')({
   maxBytes: 2000000
 });
 
-function sendData()
+function sendSerialData()
 {
   // read data from serial
   var data = {
+        id: commander.id,
 	open: ((Math.random()+100)%2),
 	temp: ((Math.random()+100) % 100),
         dispensers:
@@ -51,4 +53,38 @@ function sendData()
   });
 }
 
-kafkaesque.tearUp( function(){ setInterval(sendData, 1000*1); });
+function sendRandomData()
+{
+  // read data from serial
+  var data = {
+        id: commander.id,
+        open: ((Math.random()+100)%2),
+        temp: ((Math.random()+100) % 100),
+        dispensers:
+        {
+          "0": ((Math.random()+100)%2),
+          "1": ((Math.random()+100)%2),
+          "2": ((Math.random()+100)%2),
+          "3": ((Math.random()+100)%2),
+          "4": ((Math.random()+100)%2),
+          "5": ((Math.random()+100)%2)
+        }
+  };
+
+  // send data over to kafka
+  kafkaesque.produce({topic: 'fridge', partition: 0},
+                     [JSON.stringify(data)],
+                     function(err, response) {
+
+    if(err)
+    {
+      console.log("err", err);
+      kafkaesque.tearDown();
+    }
+    // shutdown connection
+    console.log("response:", response);
+  });
+}
+
+if(commander.emulated) kafkaesque.tearUp( function(){ setInterval(sendRandomData, 1000*1); });
+else kafkaesque.tearUp( function(){ setInterval(sendSerailData, 1000*1); });
